ea69ad50a1d31cfd329075c01d2c30e46efb134e
Olesia Vysotina <vysotina2002@gmail.com>
2025-03-17 22:41:35 +0100

    Implement find_longest_common_word method in LongestCommonWord class

--- longest_common_prefix.py
+++ longest_common_prefix.py
@@ -4,7 +4,16 @@ from trie import Trie
 class LongestCommonWord(Trie):
 
     def find_longest_common_word(self, strings) -> str:
-        pass
+        if strings is None or len(strings) == 0:
+            return ""
+
+        prefix = strings[0]
+        for string in strings[1:]:
+            while string.startswith(prefix) is False:
+                prefix = prefix[:-1]
+                if not prefix:
+                    return ""
+        return prefix
 
 
 if __name__ == "__main__":
@@ -12,11 +21,16 @@ if __name__ == "__main__":
     trie = LongestCommonWord()
     strings = ["flower", "flow", "flight"]
     assert trie.find_longest_common_word(strings) == "fl"
+    # print(trie.find_longest_common_word(strings))
 
     trie = LongestCommonWord()
     strings = ["interspecies", "interstellar", "interstate"]
     assert trie.find_longest_common_word(strings) == "inters"
+    # print(trie.find_longest_common_word(strings))
 
     trie = LongestCommonWord()
     strings = ["dog", "racecar", "car"]
     assert trie.find_longest_common_word(strings) == ""
+    # print(trie.find_longest_common_word(strings))
+
+    print("All tests passed!")
